d4c8288e46934bbddf4033c0e2b975cb49e968c0
Franchesca26 <78986079+Franchesca26@users.noreply.github.com>
2021-03-01 20:39:43 -0400

    Array 2
    Luisa Franchesca Rosario 2019-9012

---
+++ Prog 02.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Array_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] numeros = new float[5];
+
+            Console.WriteLine("Escribe 5 numero");
+
+            for (int i = 0; i < 5; i++)
+            {
+                numeros[i] = float.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("La forma inversa");
+
+            for (int i = 4; i >= 0; i--)
+            {
+                Console.WriteLine(numeros[i]);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}

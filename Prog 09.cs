c3de8da3caa698934f1e3611e859ac723a522202
Franchesca26 <78986079+Franchesca26@users.noreply.github.com>
2021-03-01 20:59:19 -0400

    Array 9
    Luisa Franchesca Rosario 2019-9012

---
+++ Prog 09.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Array_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TablaMultiplicar tm = new TablaMultiplicar();
+            Console.WriteLine("escribe un numero entero:");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("Tabla de multiplicacion de " + num);
+            tm.Calcular(num);
+            tm.CargarValor();
+
+        }
+
+    }
+    class TablaMultiplicar
+    {
+        public void CargarValor()
+        {
+            int valor;
+            string linea;
+
+            do
+            {
+                Console.Write("Ingrese un valor (-1 para finalizar):");
+                linea = Console.ReadLine();
+                valor = int.Parse(linea);
+                if (valor != -1)
+                {
+                    Calcular(valor);
+                }
+            } while (valor != -1);
+        }
+
+        public void Calcular(int v)
+        {
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(v + "x" + i + "=" + (i * v));
+            }
+
+            Console.WriteLine();
+
+        }
+    }
+}

5ea96a2204e06eb4e0e6cf7a4d992f0211efb076
Franchesca26 <78986079+Franchesca26@users.noreply.github.com>
2021-03-01 20:48:50 -0400

    Array 4
    Luisa Franchesca Rosario 2019-9012

---
+++ Prog 04.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Array_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Num = new int[10];
+            Console.WriteLine("Escribe 10 numeros");
+            for (int i = 0; i < 10; i++)
+            {
+                Num[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("el numero mayor");
+
+
+            Console.WriteLine(Num.Max());
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}

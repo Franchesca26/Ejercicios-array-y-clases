973d451054194e458c82b68110f8f35c97470ed2
Franchesca26 <78986079+Franchesca26@users.noreply.github.com>
2021-03-01 20:50:31 -0400

    Array 5
    Luisa Franchesca Rosario 2019-9012

---
+++ Prog 05.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Array_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Nombres = new string[3];
+            int K = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Escribe un nombre:");
+                Nombres[i] = Console.ReadLine();
+                K++;
+                if (Nombres[i] == "") { break; }
+
+            }
+            Console.WriteLine("\nLista de los nombres:");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(Nombres[i]);
+            }
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}

cd397d8b23b88a31fef7c618a002f466d7e3d6be
Franchesca26 <78986079+Franchesca26@users.noreply.github.com>
2021-03-01 20:56:58 -0400

    Array 7
    Luisa Franchesca Rosario 2019-9012

---
+++ Prog 07.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Array_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe tu nombre:");
+            string nombre = Console.ReadLine();
+            int longitud = nombre.Length;
+            Console.WriteLine("La division del nombre de {0} tiene {1} caracteres:", nombre, longitud);
+            for (int i = 0; i < longitud; i++)
+            {
+                Console.Write(" {0}", nombre[i]);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
